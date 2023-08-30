2982e617f6688775bac142e64816167205f077f8
unknown <danleviobello@gmail.com>
2023-08-30 17:32:22 -0500

    Fixed Product model, finished defining the Product Tag and Tag models.

--- models/Product.js
+++ models/Product.js
@@ -9,21 +9,33 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
     product_name: {
       type: DataTypes.STRING(30),
       allowNull: false
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isDecimal:true
+      }
     },
     stock: {
       type: DataTypes.INTEGEER,
-      allowNull: false
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric:true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'category',
         key: 'id',
