1962cac2199fa1e506729087ea807f8f3e4bbe4f
unknown <danleviobello@gmail.com>
2023-08-31 17:36:02 -0500

    Worked on the /category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,10 +33,39 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req, body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+
+    if(!categoryData) {
+      res.status(404).json({message: 'This is not the category you are looking for.'});
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({message: 'This is not the category you are looking for.'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
